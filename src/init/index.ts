c7926443e3bd69f2fb9fab37fad2250f95dae945
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-09-03 14:32:31 -0400

    Update shader initialization functions
    - Add 'compileShader' function that compiles directly from source
    - Rename 'compileFromDocument' -> 'compileFromScriptTag'
    - Tidy documentation
    - Deprecate 'initShaders' function

--- src/init/index.ts
+++ src/init/index.ts
@@ -1,5 +1,67 @@
 /**
- * Creates a single shader object of the given type by loading a `.glsl` file with an AJAX request.
+ * Strips an info log and wraps it with \```backticks\```. A tiny bit of extra processing is needed
+ * to get it to print nice since WebGL returns a string with a NUL byte it in.
+ * @param infoLog The info log, directly as returned by WebGL.
+ */
+function formatInfoLog(infoLog: string | null): string {
+    if (infoLog === null) {
+        return '[INFO LOG MISSING?]';
+    } else {
+        return '```\n' + infoLog.replace(/\u0000/g, '').trim() + '\n```';
+    }
+}
+
+
+/**
+ * Compiles a WebGL shader object from source.
+ *
+ * @param gl The WebGL rendering context from your canvas.
+ *
+ * @param shaderType The type of shader to compile. Must be one of
+ * {@link WebGL2RenderingContext.VERTEX_SHADER `gl.VERTEX_SHADER`} or
+ * {@link WebGL2RenderingContext.VERTEX_SHADER `gl.FRAGMENT_SHADER`}.
+ *
+ * @param shaderSrc The source code of the shader as a string.
+ *
+ * @returns The compiled shader object.
+ */
+export function compileShader(
+    gl: WebGL2RenderingContext,
+    shaderType: GLenum,
+    shaderSrc: string,
+): WebGLShader {
+    let typeName; // Used for printing errors
+    switch (shaderType) {
+        case gl.VERTEX_SHADER: typeName = 'vertex'; break;
+        case gl.FRAGMENT_SHADER: typeName = 'fragment'; break;
+        default:
+            throw new Error(
+                "Invalid `shaderType`!\n" +
+                "Should be one of `gl.VERTEX_SHADER` or `gl.FRAGMENT_SHADER`."
+            );
+    }
+
+    const shader = gl.createShader(shaderType);
+
+    if (!shader) {
+        throw new Error("Failed to initialize WebGL shader object.");
+    }
+
+    gl.shaderSource(shader, shaderSrc);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        const log = formatInfoLog(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        throw new Error(`Failed to compile ${typeName} shader. Info log:\n${log}`);
+    }
+
+    return shader;
+}
+
+
+/**
+ * Creates a single shader object of the given type by loading a source file with an AJAX request.
  *
  * ## Example
  *
@@ -22,35 +84,27 @@
  * @note This function will **not** work on the `file://` protocol. You **must** be using an actual
  * server to use `fetch` (be it a local server or an actual deployment).
  *
- * @param gl Your WebGL context object.
+ * @param gl The WebGL rendering context from your canvas.
  *
- * @param shaderType Which type of shader to create. One of `gl.VERTEX_SHADER` or
- * `gl.FRAGMENT_SHADER`.
+ * @param shaderType The type of shader to compile. Must one of
+ * {@link WebGL2RenderingContext.VERTEX_SHADER `gl.VERTEX_SHADER`} or
+ * {@link WebGL2RenderingContext.VERTEX_SHADER `gl.FRAGMENT_SHADER`}.
  *
  * @param shaderURL The path/URL from which to `fetch` the given shader from.
  *
- * @returns A single compiled shader.
- *
+ * @returns The compiled shader object.
  */
 export async function compileShaderFromURL(
     gl: WebGL2RenderingContext,
-    shaderType: WebGL2RenderingContext['FRAGMENT_SHADER'] | WebGL2RenderingContext['VERTEX_SHADER'],
+    shaderType: GLenum,
     shaderURL: string,
 ): Promise<WebGLShader> {
-    // For printing.
-    const typeName = shaderType == gl.VERTEX_SHADER ? 'vertex' : 'fragment';
-
     // Check that we're not on `file://` or some other unsupported protocol
     if (!/^http/.test(window.location.protocol)) {
         throw new Error(
             'Cannot execute `fetch` on non-HTTP protocols!\n' +
             'You need to host a web server to be able to fetch shaders as file.'
         );
-    }
-
-    const shader = gl.createShader(shaderType);
-    if (!shader) {
-        throw new Error(`Failed to create WebGL ${typeName} shader altogether. GLHF.`);
     }
 
     const shaderSrc = await fetch(shaderURL, {
@@ -60,21 +114,17 @@ export async function compileShaderFromURL(
         },
     }).then(response => response.ok ? response.text() : Promise.reject(response));
 
-    gl.shaderSource(shader, shaderSrc);
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        const log = gl.getShaderInfoLog(shader);
-        throw new Error('Shader info log:\n' + formatInfoLog(log));
-    }
-
-    return shader;
+    return compileShader(gl, shaderType, shaderSrc);
 }
 
 
 /**
  * Creates a single shader object by pulling its source code and type from the current HTML
  * document.
+ *
+ * This is a niche use-case. For the most part, you should prefer using {@linkcode compileShader} to
+ * compile an imported source-code string or {@linkcode compileShaderFromURL} to fetch it from a
+ * file on your server.
  *
  * ## Example
  *
@@ -91,10 +141,10 @@ export async function compileShaderFromURL(
  *
  * ```js
  * // using an ID:
- * const fShader = compileShaderFromDocument(gl, 'frag-shader1');
+ * const fShader = compileShaderFromScriptTag(gl, 'frag-shader1');
  * // or using the element:
  * const script = document.querySelector('script[type="x-shader/x-fragment"]');
- * const fShader = compileShaderFromDocument(gl, script);
+ * const fShader = compileShaderFromScriptTag(gl, script);
  * ```
  *
  * @param gl Your WebGL context object.
@@ -104,14 +154,11 @@ export async function compileShaderFromURL(
  * This argument may either be a string—in which case it used as an ID to select a `<script>`
  * element from the DOM—or a `<script>` element that you have already grabbed from the DOM.
  *
- * @param shaderType Which type of shader to create. One of `gl.FRAGMENT_SHADER` or
- * `gl.VERTEX_SHADER`. If omitted, this function attempts to find one of `x-shader/x-vertex` or
- * `x-shader/x-fragment` in the script tag's `type` attribute.
+ * @returns The compiled shader object.
  */
-export function compileShaderFromDocument(
+export function compileShaderFromScriptTag(
     gl: WebGL2RenderingContext,
     scriptElem: string | HTMLScriptElement,
-    shaderType?: WebGL2RenderingContext['VERTEX_SHADER'] | WebGL2RenderingContext['FRAGMENT_SHADER'],
 ): WebGLShader {
     // Get script tag
     if (typeof scriptElem === 'string') {
@@ -126,39 +173,21 @@ export function compileShaderFromDocument(
     }
 
     // Determine which shader we're making
-    if (shaderType === undefined) {
-        switch (scriptElem.type) {
-            case 'x-shader/x-vertex': shaderType = gl.VERTEX_SHADER; break;
-            case 'x-shader/x-fragment': shaderType = gl.FRAGMENT_SHADER; break;
-            default:
-                throw new Error(
-                    "Shader's script tag has unknown type!\n" +
-                    "Add `type=\"x-shader/x-vertex\" or \"x-shader/x-fragment\" to the element, " +
-                    "or pass `gl.VERTEX_SHADER`/`gl.FRAGMENT_SHADER` directly."
-                );
-        }
+    let shaderType: GLenum | null = null;
+    switch (scriptElem.type) {
+        case 'x-shader/x-vertex': shaderType = gl.VERTEX_SHADER; break;
+        case 'x-shader/x-fragment': shaderType = gl.FRAGMENT_SHADER; break;
+        default:
+            throw new Error(
+                "Shader's script tag has unknown type!\n" +
+                "Add `type=\"x-shader/x-vertex\" or \"x-shader/x-fragment\" to the element, " +
+                "or pass a `shaderType` parameter directly."
+            );
     }
-
-    // For printing.
-    const typeName = shaderType == gl.VERTEX_SHADER ? 'vertex' : 'fragment';
 
     // Get the tag's contents and finally compile
     const shaderSrc = scriptElem.textContent!.trim(); // only null if el is a document/doctype
-    const shader = gl.createShader(shaderType);
-
-    if (!shader) {
-        throw new Error(`Failed to create WebGL ${typeName} shader altogether. GLHF.`);
-    }
-
-    gl.shaderSource(shader, shaderSrc);
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        const log = gl.getShaderInfoLog(shader);
-        throw new Error('Shader info log:\n' + formatInfoLog(log));
-    }
-
-    return shader;
+    return compileShader(gl, shaderType, shaderSrc);
 }
 
 
@@ -167,11 +196,15 @@ export function compileShaderFromDocument(
  *
  * This allows you to compile your shaders however you'd like (say if you wanted to construct their
  * source-code out of smaller strings before compiling them yourself) or to make your own
- * optimizations. For the most part, you'll be using {@link compileShaderFromDocument} or
- * {@link compileShaderFromURL} and feeding their return values into this function.
+ * optimizations. For the most part, you'll be using {@linkcode compileShader} or
+ * {@linkcode compileShaderFromURL} and feeding their return values into this function.
+ *
  * @param gl Your WebGL context object.
- * @param vertShaderFilepath The path to your vertex shader file.
- * @param fragShaderFilepath The path to your fragment shader file.
+ *
+ * @param vertShader An already-compiled vertex shader object.
+ *
+ * @param fragShader An already-compiled fragment shader object.
+ *
  * @returns A linked program made from the two provided shaders.
  */
 export function linkProgram(
@@ -190,28 +223,33 @@ export function linkProgram(
     gl.linkProgram(program);
 
     if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        window.alert('Failed to link program. See devtools console for details.');
-        throw new Error('Program info log:\n' + formatInfoLog(gl.getProgramInfoLog(program)));
+        const log = formatInfoLog(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+        throw new Error(`Failed to link shader program. Info log:\n ${log}`);
     }
 
-    // Once both shaders have been linked into a program, we don't need to keep them around.
     gl.detachShader(program, vertShader);
     gl.detachShader(program, fragShader);
-    gl.deleteShader(vertShader);
-    gl.deleteShader(fragShader);
 
     return program;
 }
 
 
 /**
- * A wrapper function for calling {@link compileShaderFromDocument} and {@link linkProgram} all in
- * one step.
+ * A wrapper function for calling {@linkcode compileShaderFromScriptTag} and {@linkcode linkProgram}
+ * all in one step.
  *
- * This function corresponds to the old `initShaders` function from the original MV library.
+ * @deprecated This function is provided for backwards compatibility with the old MV library from
+ * the textbook code, which requires having your shaders available as `<script>` tags. You should
+ * prefer using {@linkcode compileShader} or {@linkcode compileShaderFromURL} to compile your own
+ * shader objects, in conjunction with {@linkcode linkProgram} to link them together.
+ *
  * @param gl Your WebGL context object.
+ *
  * @param vertShader The ID of the vertex shader's `<script>` element, or the element itself.
+ *
  * @param fragShader The ID of the fragment shader's `<script>` element, or the element itself.
+ *
  * @returns A compiled and linked program.
  */
 export function initShaders(
@@ -219,21 +257,7 @@ export function initShaders(
     vertShader: string | HTMLScriptElement,
     fragShader: string | HTMLScriptElement,
 ): WebGLProgram {
-    const vsCompiled = compileShaderFromDocument(gl, vertShader, gl.VERTEX_SHADER);
-    const fsCompiled = compileShaderFromDocument(gl, fragShader, gl.FRAGMENT_SHADER);
+    const vsCompiled = compileShaderFromScriptTag(gl, vertShader);
+    const fsCompiled = compileShaderFromScriptTag(gl, fragShader);
     return linkProgram(gl, vsCompiled, fsCompiled);
-}
-
-
-/**
- * Strips an info log and wraps it with \```backticks\```. A tiny bit of extra processing is needed
- * to get it to print nice since WebGL returns a string with a NUL byte it in.
- * @param infoLog The info log, directly as returned by WebGL.
- */
-function formatInfoLog(infoLog: string | null): string {
-    if (infoLog === null) {
-        return '[INFO LOG MISSING?]';
-    } else {
-        return '```\n' + infoLog.replace(/\u0000/g, '').trim() + '\n```';
-    }
 }
